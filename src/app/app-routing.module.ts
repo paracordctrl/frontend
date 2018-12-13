ebdc2d42cf050e92be7519dc107ec05821415a80
DESKTOP-3VCJLTV\papna <papnanan13@gmail.com>
2018-12-13 08:35:16 +0700

    add gallery and about and product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IntroComponent } from './intro/intro.component';
 import { GalleryComponent } from './gallery/gallery.component';
-import { ContentComponent } from './content/content.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
-import { ClientsComponent } from './clients/clients.component';
-import { PricingComponent } from './pricing/pricing.component';
 import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
@@ -15,10 +12,7 @@ const routes: Routes = [
   { path: 'Home', component: HeaderComponent },
   { path: 'About', component: IntroComponent },
   { path: 'Gallery', component: GalleryComponent },
-  { path: 'Services', component: ContentComponent },
   { path: 'Testimonials', component: TestimonialComponent },
-  { path: 'Clients', component: ClientsComponent },
-  { path: 'Pricing', component: PricingComponent },
   
 ];
 
--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
